113b3e23d08456910675cfc9bda97b922353e942
Adrien Brunel <adrien.brunel.pro@protonmail.com>
2025-08-11 13:41:20 +0200

    separate test by classes

---
+++ test/test_gps_collection.py
@@ -0,0 +1,106 @@
+# ======================================================= #
+# LIBRARIES
+# ======================================================= #
+import os
+import csv
+import pandas as pd
+from cpforager import parameters, utils, GPS, GPS_Collection
+
+
+# ======================================================= #
+# DIRECTORIES
+# ======================================================= #
+root_dir = os.getcwd()
+data_dir = os.path.join(root_dir, "data")
+test_dir = os.path.join(root_dir, "test")
+
+
+# ======================================================= #
+# PARAMETERS
+# ======================================================= #
+
+# set data parameters
+fieldworks = ["PER_PSC_2012_11", "PER_PSC_2013_11", "BRA_FDN_2016_09", "BRA_FDN_2018_09", "BRA_SAN_2022_03"]
+colonies = ["PER_PSC_PSC", "PER_PSC_PSC", "BRA_FDN_MEI", "BRA_FDN_MEI", "BRA_SAN_FRA"]
+
+# get parameters structure
+plot_params = parameters.get_plot_params()
+
+
+# ======================================================= #
+# TEST GPS_COLLECTION CLASS
+# ======================================================= #
+
+# loop over fieldworks
+gps_collection_all = []
+for (fieldwork, colony) in zip(fieldworks, colonies):
+
+    # list of files to process
+    files = os.listdir(os.path.join(data_dir, fieldwork))
+    n_files = len(files)
+
+    # get structure of parameters
+    params = parameters.get_params(colony)
+    plot_params = parameters.get_plot_params()
+
+    # loop over files in directory
+    gps_collection = []
+    for k in range(n_files):
+
+        # set file infos
+        file_name = files[k]
+        file_id = file_name.replace(".csv", "")
+        file_path = os.path.join(data_dir, fieldwork, file_name)
+
+        # load raw data
+        df = pd.read_csv(file_path, sep=",")
+
+        # produce "datetime" column of type datetime64
+        df["datetime"] = pd.to_datetime(df["date"] + " " + df["time"], format="mixed", dayfirst=False)
+
+        # if time is at UTC, convert it to local datetime
+        if "_UTC" in file_name: df = utils.convert_utc_to_loc(df, params.get("local_tz"))
+
+        # build GPS object
+        gps = GPS(df=df, group=fieldwork, id=file_id, params=params)
+
+        # append gps to the overall collections
+        gps_collection.append(gps)
+        gps_collection_all.append(gps)
+
+    # plot data summary
+    gps_collection = GPS_Collection(gps_collection)
+
+    # test built-in methods
+    print(gps_collection)
+    print(len(gps_collection))
+    print(gps_collection[2])
+
+    # test display_data_summary method
+    gps_collection.display_data_summary()
+
+    # test plot_stats_summary, folium_map, maps_diag methods
+    _ = gps_collection.plot_stats_summary(test_dir, "trip_statistics_%s" % fieldwork, plot_params)
+    _ = gps_collection.folium_map(test_dir, "folium_%s" % fieldwork)
+    _ = gps_collection.maps_diag(test_dir, "maps_%s" % fieldwork, plot_params)
+
+# analysis of all data
+gps_collection_all = GPS_Collection(gps_collection_all)
+
+# test built-in methods
+print(gps_collection_all)
+print(len(gps_collection_all))
+print(gps_collection_all[5])
+
+# test display_data_summary method
+gps_collection_all.display_data_summary()
+
+# test plot_stats_summary, folium_map, maps_diag methods
+_ = gps_collection_all.plot_stats_summary(test_dir, "trip_statistics_all", plot_params)
+_ = gps_collection_all.folium_map(test_dir, "folium_all")
+gps_collection_all.trip_statistics_all.to_csv("%s/trip_statistics_all.csv" % (test_dir), index=False, quoting=csv.QUOTE_NONNUMERIC)
+
+
+# ======================================================= #
+# TEST SEABIRD TRACKING DATABASE
+# ======================================================= #
